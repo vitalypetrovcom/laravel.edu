cced41dbde78ea8aad2bbebf1ea4ac8c5ff1f619
Vitaly <vitaly.petrov.com@gmail.com>
2023-11-10 18:09:31 +0300

    Урок 1.22. Валидация. Часть 2

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -12,18 +12,19 @@
 @section('content')
 
     <div class="container">
+
         <form class="mt-5" method="post" action="{{ route('posts.store') }}"> {{-- после отправки данные идут по маршруту {{ route('posts.store') }} --}}
 
             @csrf {{-- Шифрование данных --}}
 
             <div class="form-group">
                 <label for="title">Title</label>
-                <input type="text" class="form-control" id="title" placeholder="Title" name="title"> {{-- Даем имя данному полю name="title", чтобы мы на сервере смогли принять данные с этого поля --}}
+                <input type="text" class="form-control" id="title" placeholder="Title" name="title" value="{{ old ('title') }}"> {{-- Даем имя данному полю name="title", чтобы мы на сервере смогли принять данные с этого поля. Старые значения, которые были записаны в форму они доступны через специальную функцию-хелпер old (аргументом мы указываем, какое именно поле мы хотим восстановить) --}}
             </div>
 
             <div class="form-group">
                 <label for="content">Content</label>
-                <textarea class="form-control" id="content" rows="5" name="content" placeholder="Content"></textarea>
+                <textarea class="form-control" id="content" rows="5" name="content" placeholder="Content">{{ old ('content') }}</textarea>
             </div>
 
             <div class="form-group">
@@ -31,7 +32,9 @@
                 <select class="form-control" id="rubric_id" name="rubric_id"> {{-- Выведение выпадающего списка рубрик в цикле --}}
                     <option>Select rubric</option>
                     @foreach($rubrics as $k => $v)
-                        <option value="{{ $k }}">{{ $v }}</option>
+                        <option value="{{ $k }}"
+                        @if(old ('rubric_id') == $k) selected @endif{{-- Если у нас значение функции old по полю 'rubric_id' равно ключу $k, тогда мы добавим аттрибут selected --}}
+                        >{{ $v }}</option>
                     @endforeach
                 </select>
             </div>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -52,6 +52,11 @@
 
 <main role="main">
 
+    <div class="container">
+        @include('layouts.errors')
+    </div>
+
+
     @yield('content')
 
 </main>
